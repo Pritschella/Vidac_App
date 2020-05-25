112e3802de13486bde59dabb1c49d0f8fac392ea
AlanOGxD <alanosvaldo88@gmail.com>
2020-05-25 17:14:26 -0500

    Commit: Se corrigieron errores de inicio de sesion

--- app/src/main/java/com/example/appvidac/inicio_sesion.java
+++ app/src/main/java/com/example/appvidac/inicio_sesion.java
@@ -1,7 +1,6 @@
 package com.example.appvidac;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,33 +14,28 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-class inicio_sesion extends AppCompatActivity {
+public class inicio_sesion extends AppCompatActivity {
     RequestQueue rq;
-    JsonRequest jrq;
-    EditText correo, contraseña;
+    EditText correo, contrasenia;
     Button Ingresar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
-        rq = Volley.newRequestQueue(getApplicationContext());
         correo = findViewById(R.id.editTextCorreo);
-        contraseña = findViewById(R.id.editTextContraseña);
+        contrasenia = findViewById(R.id.editTextContraseña);
         Ingresar = findViewById(R.id.btnIngresar);
         Ingresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                iniciarSesion("http://localhost/login/validar_usuario.php");
+                iniciarSesion("http://192.168.1.95/Vidac/validar_usuario.php");
             }
         });
     }
@@ -58,7 +52,7 @@ class inicio_sesion extends AppCompatActivity {
                 }else{
                     Toast.makeText(inicio_sesion.this, "Correo o contraseña incorrectos", Toast.LENGTH_SHORT).show();
                 }
-            }    
+            }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -69,7 +63,7 @@ class inicio_sesion extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> parametros = new HashMap<String, String>();
                 parametros.put("Correo", correo.getText().toString());
-                parametros.put("Contraseña", contraseña.getText().toString());
+                parametros.put("Contraseña", contrasenia.getText().toString());
                 return parametros;
             }
         };
