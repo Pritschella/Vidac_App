539b74bd23f9dbc2cf3b9c06fd2a5abd2878ad63
AlanOGxD <alanosvaldo88@gmail.com>
2020-05-25 15:14:34 -0500

    Commit: Se agrego funcionalidad de login

---
+++ app/src/main/java/com/example/appvidac/PaginaPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.appvidac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaginaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_principal);
+    }
+}
