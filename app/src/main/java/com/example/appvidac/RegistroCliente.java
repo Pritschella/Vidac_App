4586c88ddb8f6de78f3ab8ba2f2ecb3609a88870
AlanOGxD <alanosvaldo88@gmail.com>
2020-05-28 14:50:13 -0500

    Commit: Se comenzo el formulario de registro de servicio

--- app/src/main/java/com/example/appvidac/RegistroCliente.java
+++ app/src/main/java/com/example/appvidac/RegistroCliente.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -37,7 +38,8 @@ import java.util.Map;
 public class RegistroCliente extends AppCompatActivity implements View.OnClickListener{
     private Button btnBuscar, btnRegistrar;
     private ImageView imageView;
-    private EditText Nombre, Apellidos, Edad, Direccion, Telefono, Genero, Parentesco, Correo, Pass;
+    private EditText Nombre, Apellidos, Edad, Direccion, Telefono, Parentesco, Correo, Pass;
+    private Spinner Genero;
     private Bitmap bitmap;
 
     private int PICK_IMAGE_REQUEST = 1;
@@ -190,9 +192,8 @@ public class RegistroCliente extends AppCompatActivity implements View.OnClickLi
                 String direccion = Direccion.getText().toString();
                 String correo = Correo.getText().toString();
                 String pass = Pass.getText().toString();
-                String genero = Genero.getText().toString();
+                String genero = Genero.getSelectedItem().toString();
                 String parentesco = Parentesco.getText().toString();
-
 
                 Map<String, String> params = new Hashtable<>();
 
--- app/src/main/java/com/example/appvidac/RegistroServicio.java
+++ app/src/main/java/com/example/appvidac/RegistroServicio.java
@@ -2,10 +2,35 @@ package com.example.appvidac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class RegistroServicio extends AppCompatActivity {
+    private Button btnBuscar, btnRegistrar;
+    private ImageView imageView;
+    private EditText Nombre, Apellidos, Edad, Direccion, Telefono, Parentesco, Correo, Pass;
+    private Spinner Genero;
+    private Bitmap bitmap;
 
+    private int PICK_IMAGE_REQUEST = 1;
+    Context context;
+
+    private String UPLOAD_URL ="http://192.168.1.95/Vidac/SubirFotoServicio.php";
+    private String RegisterClientURL ="http://192.168.1.95/Vidac/RegistrarServicio.php";
+    private String KEY_IMAGEN = "Foto";
+    private String KEY_NOMBRE = "Nombre";
+    private String KEY_APELLIDO = "Apellidos";
+    private String KEY_EDAD = "Edad";
+    private String KEY_TELEFONO = "Telefono";
+    private String KEY_DIRECCION = "Direccion";
+    private String KEY_CORREO = "Correo";
+    private String KEY_PASS = "Pass";
+    private String KEY_GENERO = "Genero";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
