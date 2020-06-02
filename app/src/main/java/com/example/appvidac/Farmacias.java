c6706c0c516a9b23b327872dfa3447bc3c2a6a60
AlanOGxD <alanosvaldo88@gmail.com>
2020-06-02 05:58:31 -0500

    Commit: se configuró el Recycler de Farmacias

--- app/src/main/java/com/example/appvidac/Farmacias.java
+++ app/src/main/java/com/example/appvidac/Farmacias.java
@@ -1,14 +1,23 @@
 package com.example.appvidac;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class Farmacias extends AppCompatActivity {
+import API.BarberosAPI;
 
+public class Farmacias extends AppCompatActivity {
+    RecyclerView lista;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmacias);
+
+        lista = (RecyclerView)findViewById(R.id.lista);
+
+        BarberosAPI data = new BarberosAPI(lista, this.getApplicationContext());
+        data.execute();
     }
 }
