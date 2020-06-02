c9a564151dd17f54b7833ecdb3c7cbb75321e12f
AlanOGxD <alanosvaldo88@gmail.com>
2020-06-02 06:52:34 -0500

    Commit: se configuró el Recycler de Restaurantes

--- app/src/main/java/com/example/appvidac/Restaurantes.java
+++ app/src/main/java/com/example/appvidac/Restaurantes.java
@@ -1,14 +1,22 @@
 package com.example.appvidac;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class Restaurantes extends AppCompatActivity {
+import API.RestaurantesAPI;
 
+public class Restaurantes extends AppCompatActivity {
+    RecyclerView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurantes);
+
+        lista = (RecyclerView)findViewById(R.id.lista);
+
+        RestaurantesAPI data = new RestaurantesAPI(lista, this.getApplicationContext());
+        data.execute();
     }
 }
